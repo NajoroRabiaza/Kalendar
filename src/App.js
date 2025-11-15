0852db9a42a159db69496d7ee61e397ed523f761
NajoroRabiaza <hei.najoro.2@gmail.com>
2025-11-15 20:26:17 +0300

    (style): set in of display on line space and space on bottom

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import googleCalendarPlugin from '@fullcalendar/google-calendar';
+import frLocale from '@fullcalendar/core/locales/fr';
 
 const VOTRE_CLE_API_GOOGLE = "AIzaSyCHFTEMzBJXWu5cOz4ADvSq8HhW4-cIz84";
 const VOTRE_ID_CALENDRIER = "jorojanah@gmail.com";
 
 function App() {
+
+  useEffect(() => {
+    setTimeout(() => {
+      window.dispatchEvent(new Event("resize"));
+    }, 50);
+  }, []);
+
   return (
-    <div style={{ padding: '20px' }}>
+    <div style={{ padding: '20px'}}>
       <FullCalendar
         plugins={[
           dayGridPlugin,
           timeGridPlugin,
           googleCalendarPlugin
         ]}
+
         initialView="timeGridWeek"
+
+        
+        height="auto"
+        contentHeight="auto"
+        expandRows={true}
+        handleWindowResize={true}
+
+        locales={[frLocale]}
+        locale="fr"
+        firstDay={1}
+
+        slotLabelFormat={{
+          hour: '2-digit',
+          minute: '2-digit',
+          hour12: false
+        }}
 
         googleCalendarApiKey={VOTRE_CLE_API_GOOGLE}
         eventSources={[
           {
             googleCalendarId: VOTRE_ID_CALENDRIER,
-            className: "google-event" // pour le styling custom
+            className: "google-event"
           }
         ]}
 
@@ -38,7 +63,6 @@ function App() {
         slotMinTime="06:00:00"
         slotMaxTime="22:00:00"
         allDaySlot={false}
-
         eventDisplay="block"
       />
     </div>
