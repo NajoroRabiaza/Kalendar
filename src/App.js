d2346ee8a9517e0c2c8d06ac3af479efb5d94482
NajoroRabiaza <hei.najoro.2@gmail.com>
2025-11-16 05:26:58 +0300

    (style): the display color of the ecent is very good now

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ import "./App.css";
 
 const API_KEY = "AIzaSyCHFTEMzBJXWu5cOz4ADvSq8HhW4-cIz84";
 
-// sources a modifier selon le calendrier et selon le couleurs
+// les sources a lier avec les calendrier creer
 const SOURCES = [
   { id: "groupeA", title: "H1", calendarId: "5a7d67665a2a4f9947883ae151366043ac09e229a04a0381e0b6e2476d1f64d2@group.calendar.google.com", color: "#FF6B6B" },
   { id: "groupeB", title: "H4", calendarId: "923dd993f1ce96964d16621b06a46ef229bc747cc1f58a4302cfb8531629f855@group.calendar.google.com", color: "#999999" },
-  { id: "groupeC", title: "G3", calendarId: " b52757b92f0708d42f6642f0bc602fb5fcdb38e29bbdd4e1bee34368cc153795@group.calendar.google.com", color: "#9B5DE5" },
+  { id: "groupeC", title: "G3", calendarId: "b52757b92f0708d42f6642f0bc602fb5fcdb38e29bbdd4e1bee34368cc153795@group.calendar.google.com", color: "#9B5DE5" },
 ];
 
 // mapping utile pour fallback (className -> couleur)
